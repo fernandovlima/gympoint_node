f1922e0f4a35426601c5ef46b8e3bb7fa4610ae5
fernandovlima <fernandovlima.s@gmail.com>
2019-10-17 13:53:53 -0300

    model user and migration

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Gympoint' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Fernando Lima',
+    email: 'fernandovlima.s@gmail.com',
+    password_hash: '131313',
+  });
+  return res.json(user);
 });
 
 export default routes;
