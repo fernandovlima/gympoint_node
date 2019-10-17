27b624b8f218b0eac803276714ee05aefd8edeb7
fernandovlima <fernandovlima.s@gmail.com>
2019-10-17 12:08:51 -0300

    .

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  res.json({ message: "Gympoint" });
+  return res.json({ message: "Gympoint" });
 });
 
 export default routes;
