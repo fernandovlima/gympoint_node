8f19eedf8228653433cf2247b5c775449af6f0b3
fernandovlima <fernandovlima.s@gmail.com>
2019-10-21 18:48:04 -0300

    add Registration model and migration

---
+++ src/database/migrations/20191021210722-create-registrations.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('registrations', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      start_date: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      end_date: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'students', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      plan_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'plans', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('registrations');
+  },
+};
