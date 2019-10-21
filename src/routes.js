0949ceae5cf76a9d8f2e5cec08253b722646bdf2
fernandovlima <fernandovlima.s@gmail.com>
2019-10-21 17:57:20 -0300

    add Plan model and migration

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ routes.post('/sessions', SessionController.store);
 // middleware de auth apenas para as rotas abaixo de onde esta declarado.
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
 routes.post('/students', StudentsController.store);
 routes.put('/students', StudentsController.update);
 
